31c619ad438ca0ca2e1c0b03c1b4945ede74f832
Alexandre TOBO DELALEU <none@none>
2025-04-05 13:40:49 +0200

    fix: wagmi use now the proper sepolia rpc

--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,6 +1,5 @@
 import { createPublicClient, http } from 'viem'
-import { hardhat } from 'viem/chains'
-import { sepolia } from '@/utils/sepolia'
+import { hardhat, sepolia } from 'viem/chains'
 
 export const publicClient = createPublicClient({
     chain: process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? sepolia : hardhat,
